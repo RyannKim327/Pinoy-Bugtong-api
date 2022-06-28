53785b6775d0bcdc411ec89cc46d5d47d8bc404f
RyannKim327 <rksesgundo123@gmail.com>
2022-06-29 00:28:52 +0800

    06-29-22 00:28

--- index.js
+++ index.js
@@ -4,7 +4,41 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-	res.send("Sample")
+	const a = [
+		{
+			"b": atob("QmFib3kga28gc2EgcHVsbywgYmFsYWhpYm8neSBwYWtv"),
+			"s": atob("bGFuZ2th") // 1
+		},
+		{
+			"b": atob("QnV0byd0IGJhbGF0LCBsdW1pbGlwYWQ="),
+			"s": atob("c2FyYW5nZ29sYQ==") // 2
+		},
+		{
+			"b": atob("QXlhbiBuYSwgYXlhbiBuYSwgaGluZGkgbW8gcGEgbWFraXRh"),
+			"s": atob("aGFuZ2lu") // 3
+		},
+		{
+			"b": atob("SGluZGkgaGFyaSwgaGluZGkgcGFyaSwgbmd1bml0IGFuZyBzdW90IGF5IHNhcmktc2FyaQ=="),
+			"s": atob("c2FtcGF5YW4=") // 4
+		},
+		{
+			"b": atob("RGluaWRpbGFhbiBtbyBtdW5hLCBiYWdvIGlwYXNvaw=="),
+			"s": atob("c2ludWxpZA==") // 5
+		},
+		{
+			"b": atob("TWFiYWJhIGthcGFnIG5ha2EtdGF5bywgbWF0YWFzIGthcGFnIG5ha2F1cG8="),
+			"s": atob("YXNv") // 6
+		},
+		{
+			"b": atob("SXNkYSBrbyBzYSBNYXJpdmVsZXMsIG5hc2EgbG9vYiBhbmcga2FsaXNraXM="),
+			"s": atob("c2lsaQ==") // 7
+		},
+		{
+			"b": atob("U2EgdW1hZ2EneSBtYXkgYXBhdCBuYSBwYWEsIHNhIHRhbmdoYWxpJ3kgZGFsYXdhIG5hLCBhdCBzYSBwYWdzYXBpdCBuZyBoYXBvbiBheSB0YXRsbyBuYS4="),
+			"s": atob("dGFv") // 8
+		}
+	]
+	res.send(a[Math.floor(Math.random() * a.length)])
 })
 
 app.listen(port, () => {
